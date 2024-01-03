29ba8cde05fb0e75c36af7dcffb092e756e8444e
bybymaxy <141274790+bybymaxy@users.noreply.github.com>
2024-01-02 20:13:48 -0800

    fixed a few path issues updated my .env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const helpers = require('./utils middleware/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -53,3 +53,14 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    // Process the data returned by the API
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the API call
+    console.error(error);
+  });
