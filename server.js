3e5ca8d1e50479d1aa623dd61561799ecd4909ef
Bjpippenger <144498503+Bjpippenger@users.noreply.github.com>
2024-01-02 20:43:28 -0800

    renamed routes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const axios = require('axios'); // Import Axios
 const routes = require('./controllers');
-const helpers = require('./utils middleware/helpers.js');
+const helpers = require('./utils/helpers.js');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
