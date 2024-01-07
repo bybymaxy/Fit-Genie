9517a86a238273d389f81a80708d150562be24b4
Bjpippenger <144498503+Bjpippenger@users.noreply.github.com>
2024-01-06 21:34:20 -0800

    update

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,13 @@
-
 DROP DATABASE IF EXISTS fitness_app;
-
-
 CREATE DATABASE fitness_app;
-
-
 USE fitness_app;
 
+CREATE TABLE user (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE workouts (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -15,7 +16,6 @@ CREATE TABLE workouts (
   duration INT,
   calories_burned INT
 );
-
 
 CREATE TABLE workout_logs (
   id INT AUTO_INCREMENT PRIMARY KEY,
