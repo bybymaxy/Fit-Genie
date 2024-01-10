0e47046f3972a6bfa03dbbcb5c5edc114799ca5a
Matthew_Hill <hillmatt58@gmail.com>
2024-01-10 16:56:37 -0700

    API key working properly

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const usersController = require('./controllers/api/usersController');
 const { getUsers } = require('./controllers/api/usersController');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
