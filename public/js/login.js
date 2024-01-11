59697b69e8933d170f8b78b3d22ec0b1b5494cea
rehposolihpedoc <142445854+rehposolihpedoc@users.noreply.github.com>
2024-01-10 17:39:51 -0800

    Updated profile.handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,6 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
+  
 
   // Collect values from the login form
   const email = document.querySelector('#email-login').value.trim();
@@ -25,14 +26,14 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const username = document.querySelector('#username').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
 
-  if (name && email && password) {
+  if (username && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ username, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
@@ -44,10 +45,12 @@ const signupFormHandler = async (event) => {
   }
 };
 
-document
+/*document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
+  */
 
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
+ 
